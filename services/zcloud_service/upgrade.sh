ac9139badfa90a92516d2a50c58728fc9fa6564b
HeRenk <heqinke98@163.com>
2025-03-14 09:50:45 +0800

    安装脚本修改

--- services/zcloud_service/upgrade.sh
+++ services/zcloud_service/upgrade.sh
@@ -1,1 +1,127 @@
-echo "upgrade 1 ok"
+
+export nodeNum=#{nodeNum}
+export installNodeType=#{installNodeType}
+export installType=#{installType}
+export installPath=#{installPath}
+export homePath=#{homePath}
+export workdir=#{workdir}
+export osType=#{osType}
+export osVersion=#{osVersion}
+export oldRelease=#{oldRelease}
+export release=#{release}
+export realHostIp=#{hostIp}
+export theme=#{theme}
+export databaseType=#{databaseType}
+export mysqluser=#{mysqluser}
+export mysqlpassword=#{mysqlpassword}
+export mysqlhost=#{mysqlhost}
+export mysqlhostport=#{mysqlhostport}
+export mogdbuser=#{mogdbuser}
+export mogdbpassword=#{mogdbpassword}
+export mogdbport=#{mogdbport}
+export mogdbhost=#{mogdbhost}
+export logPath="${homePath}/dbaas/zcloud-log"
+export logFile="${homePath}/dbaas/zcloud-log/install.log"
+export packagePath="${homePath}/dbaas/soft-package"
+export bakPath="${homePath}/dbaas/soft-bak"
+export configPath="${homePath}/dbaas/zcloud-config"
+export javaIoTempDir="${logPath}/java-io-tmpdir"
+export executeUser=#{executeUser}
+
+. ./script/lib/common.sh
+. ./services/zcloud_service/zcloud_server_install.sh
+. ./services/zcloud_service/monitor_component_install_unroot.sh
+
+
+export ipPath=($( __ReadValue ${logPath}/evn.cfg ipPath))
+export ssPath=($( __ReadValue ${logPath}/evn.cfg ssPath))
+export oldVersion1=($( __ReadValue ${logPath}/evn.cfg oldVersion))
+export bakTime=($( __ReadValue ${logPath}/evn.cfg bakTimeS))
+
+export serviceName=$1
+
+function __UpgradeZcloudService() {
+
+. ./script/lib/common.sh
+. ./services/zcloud_service/zcloud_server_install.sh
+. ./services/zcloud_service/monitor_component_install_unroot.sh
+
+  if [[ ${theme} == "zData" ]];then
+    theme=zData
+    databaseType=MySQL
+    hostIp=127.0.0.1
+    sed -ri  "s|mysql.root.paasword=.*|mysql.root.paasword=zdata_2019|g" ${workdir}/zcloud.cfg
+  fi
+  cd ${workdir}
+
+  monitorComponent=("node-exporter","alertmanager","zoramon-mgr","smart-baseline","dbaas-mail-sender","dbaas-wxwork-sender","dbaas-sender-common","dbaas-zabbix-sender","slowmon_mgr")
+  for item in "${monitorComponent[@]}";
+    do
+      if [[ "$item" == "$serviceName" ]]; then
+          h2 "[安装监控组件 ... ${serviceName}";
+              startTime=$(date +"%s%N")
+              __InstallMonitorComponent
+              endTime=$(date +"%s%N")
+          echo "安装监控组件${serviceName} 完成，耗时$( __CalcDuration ${startTime} ${endTime})"
+          break
+      fi
+    done
+
+
+  normalService=("dbaas-eureka-server" "dbaas-backend-damengdb" "dbaas-monitor" "dbaas-api-create-dg"
+  "dbaas-configuration" "dbaas-mariadb" "dbaas-db-manage" "dbaas-create-shardingsphere"
+  "dbaas-apigateway" "dbaas-infrastructure" "dbaas-operate-db" "dbaas-permissions"
+  "dbaas-reposerver" "task-management" "dbaas-database-snapshot" "dbaas-backend-mogdb"
+  "dbaas-common-db" "dbaas-lowcode-http-engine" "dbaas-management-database"
+  "dbaas-management-host")
+  for item in "${normalService[@]}";
+    do
+      if [[ "$item" == "$serviceName" ]]; then
+        __InstallNormalZcloudService
+        __CheckZcloudSingleServiceStatus
+        break
+      fi
+    done
+
+
+  if [[ ${serviceName} == "dbaas-flyway-manage" ]]; then
+    __InstallFlyway
+    __CheckZcloudSingleServiceStatus
+  fi
+
+  if [[ ${serviceName} == "zdbmon-mgr" ]]; then
+    __InstallZdbmonMgr
+    __CheckZcloudSingleServiceStatus
+  fi
+
+  if [[ ${serviceName} == "offline_health_check_collector" ]]; then
+    # 复制offline_health_check_collector 到/paasdata
+    move_collector_to_paasdata
+  fi
+
+  __ReplaceText ${logPath}/evn.cfg "realHostIp=" "realHostIp=${realHostIp}"
+}
+
+__ReplaceText ${logPath}/evn.cfg "step=" "step=${item}"
+ipPath=`which ip`
+ssPath=`which ss`
+__ReplaceText ${logPath}/evn.cfg "ipPath=" "ipPath=${ipPath}"
+__ReplaceText ${logPath}/evn.cfg "ssPath=" "ssPath=${ssPath}"
+__ReplaceText ${logPath}/evn.cfg "theme=" "theme=${theme}"
+__ReplaceText ${logPath}/evn.cfg "realHostIp=" "realHostIp=${hostIp}"
+
+if [[ ${executeUser} = "root" ]];then
+  function_call="$(declare -f __UpgradeZcloudService); __UpgradeZcloudService"
+  su --preserve-environmen zcloud -c "$function_call"
+else
+  __UpgradeZcloudService
+fi
+
+item=($( __ReadValue ${logPath}/evn.cfg step))
+realHostIp=($( __ReadValue ${logPath}/evn.cfg realHostIp))
+
+
+if [[ ${serviceName} == "changeZcloudCfg" ]]; then
+  #修改zcloud配置文件内容
+  __ChangeZcloudCfg
+fi
